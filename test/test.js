8c97755153e369b83375995ee2aa7774cb2646b2
DenseFog <move11@126.com>
2023-04-13 20:14:51 +0800

    chore: change

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 // commonjs
 // const  {getNextDay, getWeekByDay} = require('@dense-labs/utils')
 // es modules
-import {getNextDay, getWeekByDay, Signature} from '@dense-labs/utils'
-console.log(Signature)
+import {getNextDay, getWeekByDay, signature} from '@dense-labs/utils'
 console.log(getNextDay(10))
 console.log(getWeekByDay('2023-04-10'))
+
+new signature()
