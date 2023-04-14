88ffaece922e28247b0c7c3a898bd09de5d0be35
DenseFog <move11@126.com>
2023-04-14 17:03:48 +0800

    chore: change

---
+++ test/mian.ts
@@ -0,0 +1,13 @@
+// commonjs
+// const  {getNextDay, getWeekByDay} = require('@dense-labs/utils')
+// es modules
+import {getNextDay, getWeekByDay, Signature, isNumber} from '../dist/index.mjs'
+console.log(getNextDay(10))
+console.log(getWeekByDay('2023-04-10'))
+
+new Signature({
+    ele: 'signature'
+})
+import {isEmail, isUrl} from '@pureadmin/utils'
+
+console.log(isEmail('sds@sdsadsa.ccc'))
