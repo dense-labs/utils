d2c43ae49b97799f1bf31e7184583efef1e6b422
DenseFog <move11@126.com>
2023-04-14 11:34:49 +0800

    chore: chaneg

--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
 // commonjs
 // const  {getNextDay, getWeekByDay} = require('@dense-labs/utils')
 // es modules
-import {getNextDay, getWeekByDay, signature} from '@dense-labs/utils'
+import {getNextDay, getWeekByDay, signature} from '../dist/index.js'
 console.log(getNextDay(10))
 console.log(getWeekByDay('2023-04-10'))
 
-new signature()
+new signature({
+    ele: 'signature'
+})
+import {isEmail, isUrl} from '@pureadmin/utils'
+
+console.log(isEmail('sds@sdsadsa.ccc'))
