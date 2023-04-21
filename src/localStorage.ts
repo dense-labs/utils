cefd7523e28d69ed89b00ceaab2a4685c59cb2c3
DenseFog <move11@126.com>
2023-04-21 17:14:03 +0800

    fix: Error when exporting static packaged references in class

---
+++ src/localStorage.ts
@@ -0,0 +1,41 @@
+export type ExLocalStorageItem = {
+	value: string
+	expireTime?: number
+}
+
+export class ExLocalStorage {
+	private static readonly STORAGE_PREFIX = '__ex_local_storage_'
+
+	private static getStorageKey(key: string) {
+		return `${ExLocalStorage.STORAGE_PREFIX}${key}`
+	}
+
+	setItem(key: string, value: string, expireTime?: number) {
+		const item: ExLocalStorageItem = {value}
+		if (expireTime) {
+			item.expireTime = Date.now() + expireTime
+		}
+		localStorage.setItem(ExLocalStorage.getStorageKey(key), JSON.stringify(item))
+	}
+
+	getItem(key: string): string | null {
+		const itemStr = localStorage.getItem(ExLocalStorage.getStorageKey(key))
+		if (!itemStr) return null
+
+		const item: ExLocalStorageItem = JSON.parse(itemStr)
+		if (Date.now() > item.expireTime) {
+			localStorage.removeItem(ExLocalStorage.getStorageKey(key))
+			return null
+		}
+
+		return item.value
+	}
+
+	removeItem(key: string) {
+		localStorage.removeItem(ExLocalStorage.getStorageKey(key))
+	}
+}
+
+export function storageLocal() {
+	return new ExLocalStorage()
+}
