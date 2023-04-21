1e41e7b1e0c012eed96e65d1e74a0b8295cc659f
DenseFog <move11@126.com>
2023-04-21 17:13:54 +0800

    fix: Error when exporting static packaged references in class

--- rollup.config.js
+++ rollup.config.js
@@ -9,38 +9,40 @@ import fs from 'fs'
 import path from 'path'
 const pkg = JSON.parse(await fs.readFileSync(path.resolve(process.cwd(), './package.json'), {encoding: 'utf-8'}))
 
-export default {
-	input: 'src/**/*.ts', //入口
-	output: [
-		{
-			file: pkg.main,
-			format: 'cjs'
+export default [
+	{
+		input: 'src/index.ts', //入口
+		output: [
+			{
+				file: pkg.main,
+				format: 'cjs'
+			},
+			{
+				file: pkg.module,
+				format: 'es'
+			},
+			{
+				name: 'denseLabs',
+				file: './dist/index.umd.js',
+				format: 'umd'
+			}
+		],
+		// 声明它的外部依赖，可以不被打包进去。
+		external: [],
+		// 监听文件的变化，重新编译，只有在编译的时候开启 --watch 才生效。
+		watch: {
+			include: 'src/**'
 		},
-		{
-			file: pkg.module,
-			format: 'es'
-		},
-		{
-			name: 'denseLabs',
-			file: './dist/index.umd.js',
-			format: 'umd'
-		}
-	],
-	// 声明它的外部依赖，可以不被打包进去。
-	external: [],
-	// 监听文件的变化，重新编译，只有在编译的时候开启 --watch 才生效。
-	watch: {
-		include: 'src/**'
-	},
-	plugins: [
-		clear({
-			targets: ['dist']
-		}),
-		resolve(),
-		typescript(/* { declaration: true, declarationDir: '/' } */{removeComments: false}),
-		multiEntry(),
-		commonjs(),
-		terser(),
-		uglify()
-	]
-}
+		plugins: [
+			clear({
+				targets: ['dist']
+			}),
+			resolve(),
+			typescript(/* { declaration: true, declarationDir: '/', removeComments: false } 比tsonfig 中配置优先级高 */),
+			multiEntry(),
+			commonjs(),
+			terser(),
+			uglify()
+		]
+	}
+]
