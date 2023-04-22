f53a7bed656a46bd5461bb34a0c855b415b7ac8f
DenseFog <move11@126.com>
2023-04-22 20:08:11 +0800

    chore: change

--- test/mian.ts
+++ test/mian.ts
@@ -1,6 +1,7 @@
 // commonjs
 // const  {getNextDay, getWeekByDay} = require('@dense-labs/utils')
 // es modules
+import {isWatchMode} from 'vitest'
 import {getNextDay, getWeekByDay, Signature, isNumber} from '../dist/index.mjs'
 console.log(getNextDay(10))
 console.log(getWeekByDay('2023-04-10'))
@@ -9,5 +10,4 @@ new Signature({
     ele: 'signature'
 })
 import {isEmail, isUrl} from '@pureadmin/utils'
-
 console.log(isEmail('sds@sdsadsa.ccc'))
