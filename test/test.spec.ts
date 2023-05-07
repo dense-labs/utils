9a9834a363cbe20c723233fb456ae9f0869c39fc
DenseFog <move11@126.com>
2023-05-07 22:01:44 +0800

    feat: vitest

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,20 +1,20 @@
 import {test, expect, expectTypeOf} from 'vitest'
-import {getRandomColor, rgbToHex, rgbToRgba} from '../dist/index.mjs'
+import {getRandomColor, rgbToHex, rgbToRgba, isUndefined, isNull, isString, isNumber, isBoolean, isSymbol, isBigInt, isObject, isPlainObject, isArray, isFunction} from '../dist/index.mjs'
 test('adds 1 + 2 to equal 3', () => {
 	expect(1 + 2).toBe(3)
 })
-test('getRandomColor', (assert) => {
+test('getRandomColor', () => {
 	const color = getRandomColor()
 	expectTypeOf(color).toBeString()
 })
 
-test('rgbToHex', (assert) => {
+test('rgbToHex', () => {
 	expect(rgbToHex('rgb(123, 45, 67)') === '#7b2d43').toBe(true)
 	expect(rgbToHex('rgb(255, 255, 255)') === '#ffffff').toBe(true)
 	expect(rgbToHex('rgb(0, 0, 0)') === '#000000').toBe(true)
 })
 
-test('rgbToRgba', (assert) => {
+test('rgbToRgba', () => {
 	expect(rgbToRgba('rgb(123, 45, 67)', 0.5) === 'rgba(123, 45, 67, 0.51)').toBe(true)
 	expect(rgbToRgba('rgb(255, 255, 255)', '0.75') === 'rgba(255, 255, 255, 0.75)').toBe(true)
 	expect(rgbToRgba('rgb(0, 0, 0)', 1) === 'rgba(0, 0, 0, 1)').toBe(true)
