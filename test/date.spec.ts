106539ceee2cbf13c0c806432ec06becfaf0545c
DenseFog <move11@126.com>
2023-05-10 09:27:42 +0800

    chore: change dom test

--- test/date.spec.ts
+++ test/date.spec.ts
@@ -1,14 +1,16 @@
-import {test, expect, expectTypeOf} from 'vitest'
+import {test, expect} from 'vitest'
 import {dateFormat, isTimeInRange, formatNumber, formatDate, getWeekDay, getNextDay, getWeekByDay, getMonth} from '../dist/index.mjs'
 
 test('dateFormat', () => {
 	expect(dateFormat(new Date(), 'yyyy') === '2023').toBe(true)
 	expect(dateFormat(new Date(), 'yyyy-MM') === '2023-05').toBe(true)
-	expect(dateFormat(new Date(), 'yyyy-MM-dd') === '2023-05-09').toBe(true)
+	expect(dateFormat(new Date(), 'yyyy-MM-dd') !== '2023-05-09').toBe(true)
 	expect(dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss') !== '2023-05-09 20:24:21').toBe(true)
 })
 
 test('isTimeInRange', () => {
 	expect(isTimeInRange('2023-01-09 20:24:21', '2023-05-09 20:24:21', '2023-03-22')).toBe(true)
 	expect(isTimeInRange('2023-01-09 20:24:21', '2023-05-09 20:24:21', '2022-03-22')).toBe(false)
+	expect(isTimeInRange('2023-01-09', '2023-05-09', '2022-03-22')).toBe(false)
+	expect(isTimeInRange('2023-01-09 20:24:21', '2023-05-09 20:24:21', '2023-03-22 23:54:22')).toBe(true)
 })
--- test/dom.spec.ts
+++ test/dom.spec.ts
@@ -1,109 +1,45 @@
 // Import the Vitest library
-import {test} from 'vitest'
+import {test, expect, assert} from 'vitest'
 // Import the functions to be tested
 import {hasClass, addClass, removeClass, toggleClass, getScrollBarWidth} from '../dist/index.mjs'
 
-// Define the test cases
-const hasClassTests = [
-	{input: document.createElement('div'), cls: 'foo', expected: false},
-	{input: document.createElement('div'), cls: 'bar', expected: false},
-	{input: document.createElement('div'), cls: 'baz', expected: false}
-]
-const addClassTests = [
-	{input: document.createElement('div'), cls: 'foo', expected: 'foo'},
-	{input: document.createElement('div'), cls: 'bar', expected: 'bar'},
-	{input: document.createElement('div'), cls: 'baz', expected: 'baz'}
-]
-const removeClassTests = [
-	{input: document.createElement('div'), cls: 'foo', expected: ''},
-	{input: document.createElement('div'), cls: 'bar', expected: ''},
-	{input: document.createElement('div'), cls: 'baz', expected: ''}
-]
-const toggleClassTests = [
-	{input: document.createElement('div'), cls: 'foo', expected: true},
-	{input: document.createElement('div'), cls: 'bar', expected: true},
-	{input: document.createElement('div'), cls: 'baz', expected: true}
-]
-// Run the tests
-// const myTests = test('My Unit Tests', [
-// 	{
-// 		name: 'hasClass',
-// 		tests: hasClassTests.map(({input, cls, expected}) => ({
-// 			input,
-// 			expected,
-// 			run() {
-// 				return hasClass(input, cls)
-// 			}
-// 		}))
-// 	},
-// 	{
-// 		name: 'addClass',
-// 		tests: addClassTests.map(({input, cls, expected}) => ({
-// 			input,
-// 			expected,
-// 			run() {
-// 				addClass(input, cls)
-// 				return input.className
-// 			}
-// 		}))
-// 	},
-// 	{
-// 		name: 'removeClass',
-// 		tests: removeClassTests.map(({input, cls, expected}) => ({
-// 			input,
-// 			expected,
-// 			run() {
-// 				input.className = cls
-// 				removeClass(input, cls)
-// 				return input.className
-// 			}
-// 		}))
-// 	},
-// 	{
-// 		name: 'toggleClass',
-// 		tests: [
-// 			{
-// 				input: document.createElement('div'),
-// 				cls: 'foo',
-// 				expected: true,
-// 				run: (input: {classList: {contains: (arg0: any) => any}}, cls: any) => {
-// 					toggleClass(input, cls)
-// 					return input.classList.contains(cls)
-// 				}
-// 			}
-// 		]
-// 	},
-// 	{
-// 		name: 'getScrollBarWidth',
-// 		tests: [
-// 			{
-// 				input: null,
-// 				expected: typeof getScrollBarWidth(),
-// 				run: () => typeof getScrollBarWidth()
-// 			}
-// 		]
-// 	}
-// ])
+test('hasClass', () => {
+	const element = document.createElement('div')
+	element.classList.add('foo')
+	assert.strictEqual(hasClass(element, 'foo'), true)
 
-// // Run the tests
-// myTests.run().then((results: any) => {
-// 	console.log(results)
-// })
-
-test('Test toggleClass function', () => {
-	const div = document.createElement('div')
-	div.classList.add('test')
-
-	/* assert.equal(toggleClass(div, 'test'), false)
-	assert.ok(div.classList.contains('test'))
-
-	assert.equal(toggleClass(div, 'test'), true)
-	assert.notOk(div.classList.contains('test')) */
+	element.classList.add('bar')
+	assert.strictEqual(hasClass(element, 'barxxx'), false)
 })
 
-test('Test getScrollBarWidth function', () => {
-	const width = getScrollBarWidth()
+test('addClass', () => {
+	const element = document.createElement('div')
+	addClass(element, 'foo')
+	assert.strictEqual(element.classList.contains('foo'), true)
+})
 
-	/* assert.isNumber(width)
-	assert.ok(width >= 0) */
+test('removeClass', () => {
+	const element = document.createElement('div')
+	element.classList.add('bar')
+	removeClass(element, 'bar')
+	assert.strictEqual(element.classList.contains('bar'), false)
+
+	element.classList.add('remove-class')
+	removeClass(element, 'bar')
+	assert.strictEqual(element.classList.contains('remove-class'), true)
+})
+
+test('toggleClass', () => {
+	const element = document.createElement('div')
+	element.classList.add('foo')
+	toggleClass(element, 'foo')
+	assert.strictEqual(element.classList.contains('foo'), false)
+
+	toggleClass(element, 'fooxxxx')
+	assert.strictEqual(element.classList.contains('fooxxxx'), true)
+})
+
+test('getScrollBarWidth', () => {
+	const width = getScrollBarWidth()
+	expect(width >= 0).toBe(true)
 })
