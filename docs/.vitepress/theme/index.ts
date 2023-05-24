64f1bb15a4e4c1d4b53d7646b9443b93df37dd08
DenseFog <move11@126.com>
2023-05-24 21:06:36 +0800

    chore: add theme config

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+
+import { type Theme } from "vitepress"
+import DefaultTheme from "vitepress/theme"
+import './style/var.css'
+
+const theme: Theme = {
+    ...DefaultTheme
+};
+
+export default theme
