6285d20a9797920f766caa01cb75d32ed50cf261
DenseFog <move11@126.com>
2023-07-15 23:14:49 +0800

    chore: change test

--- test/mian.ts
+++ test/mian.ts
@@ -5,5 +5,4 @@ import {getNextDay, getWeekByDay, isTimeInRange, isEmail} from '../dist/index.mj
 console.log(getNextDay(10))
 console.log(getWeekByDay('2023-04-10'))
 console.log(isEmail('sds@sdsadsa.ccc'))
-console.log(isEmail('sds@sdsadsa.ccc'))
 console.log(isTimeInRange('2023-01-12 12:23:23', '2023-12-22 23:23:23', '2023-05-12'))
